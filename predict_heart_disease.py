7d87bc56c4e7afc7b3a28a49d75331e8cb2d2d39
Nividh S <nividh.singh@gmail.com>
2024-09-29 02:02:32 -0400

    Documented code

--- predict_heart_disease.py
+++ predict_heart_disease.py
@@ -4,17 +4,16 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 
 def compute_output(inputs):
-    hd = pd.read_csv('heart_disease_health_indicators_BRFSS2015.csv')
-    y = hd['HeartDiseaseorAttack']
-    X = hd.drop(columns=['HeartDiseaseorAttack'])
+    hd = pd.read_csv('heart_disease_health_indicators_BRFSS2015.csv')  # Load dataset
+    y = hd['HeartDiseaseorAttack']  # Target variable
+    X = hd.drop(columns=['HeartDiseaseorAttack'])  # Feature set
 
-    model = DecisionTreeClassifier()
-    model.fit(X, y)
-    test = inputs # pd.read_csv('testcase.csv')
-    X_test = test #.drop(columns=['BlankPredictor'])
-    prediction = model.predict(X_test)
-    print(prediction)
-    return prediction
-    # print("Your output value is above. A value of 0 means you do not have heart disease, while a value of 1 means you do.")
+    model = DecisionTreeClassifier()  # Initialize classifier
+    model.fit(X, y)  # Train the model
     
+    X_test = inputs  # Input data for prediction
+    prediction = model.predict(X_test)  # Make predictions
+    
+    print(prediction)  # Print predictions
+    return prediction  # Return predictions
     
